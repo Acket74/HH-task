421b9314dde5790c01b6d4948fd17c8afdede9ed
Acket74 <vpe4ko@gmail.com>
2015-06-17 01:31:40 +0300

    comment fixing

--- Task1/src/com/company/Main.java
+++ Task1/src/com/company/Main.java
@@ -3,13 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //a,b положительные числа(старший бит равен 0).
+        //a,b are positive(highest bit is 0).
         int a = 0x7ffffffc;
         int b = 0x7ffffffe;
 
         //a+b in hex format
-        //Старший бит равен единице, соответственно, число отрицательное.
-        //Значение целой части получаем XOR-ом от всего числа. То есть: 0x00000006
+        //negative(highest bit is 1)
+        //so using compliment transformation we getting a = 1010 => 0101 + 1 = 6
         int e = 0xfffffffa;
 
         int c = a + b;
